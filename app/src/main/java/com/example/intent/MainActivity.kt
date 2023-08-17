0080443df71da511660cdb30f2c77d7d79730980
Abdulbasit Oduk <a.oduk@pegb.tech>
2023-08-17 14:21:00 +0300

    Updated Xml and code side

--- app/src/main/java/com/example/intent/MainActivity.kt
+++ app/src/main/java/com/example/intent/MainActivity.kt
@@ -5,13 +5,22 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
+
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Find the button in your main activity layout
+        // Find the button in your main activity layout
+        findViewById<View>(R.id.btnOpenSecondActivity).setOnClickListener {
+            openSecondActivityTextClick(it)
+        }
     }
 
     fun openSecondActivityTextClick(view: View) {
+        // Create an intent to open the new activity
         val intent = Intent(this@MainActivity, MainActivity2::class.java)
         startActivity(intent)
     }
